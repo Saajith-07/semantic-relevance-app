6dcc543863ab1f278175799a652463d5abccfa78
Saajith-07 <saajithmkks36@gmail.com>
2025-09-10 15:30:26 +0530

    Update requirements1.py

--- requirements1.py
+++ requirements1.py
@@ -1,3 +1,5 @@
-streamlit
-torch --extra-index-url https://download.pytorch.org/whl/cpu
+torch==2.0.1+cpu
+torchvision==0.15.2+cpu
+torchaudio==2.0.2+cpu
+-f https://download.pytorch.org/whl/cpu/torch_stable.html
 sentence-transformers
